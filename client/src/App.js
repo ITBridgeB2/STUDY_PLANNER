13f086fedf7e58f1ba60ec3d00dc676ff1e5e691
Muskan Kouser A <135813964+MuskanKouser@users.noreply.github.com>
2025-06-02 11:46:11 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,18 @@
-import './App.css';
+
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import TaskList from './TaskList';
+import TaskDetails from './DetailPage';
 
 function App() {
   return (
-    <div className="App">
-      <h1>hello</h1>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Navigate to="/tasks" />} />
+        <Route path="/tasks" element={<TaskList />} />
+        <Route path="/tasks/:id" element={<TaskDetails />} />
+      </Routes>
+    </Router>
   );
 }
 
